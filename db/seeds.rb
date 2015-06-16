3afb30990b55d616a9427108972d80ede1cece6c
Kelly Raila <kelly.raila@gmail.com>
2015-06-16 15:36:49 -0400

    Experiment with a show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,7 @@
 # Example:
 #
 #   Person.create(first_name: 'Eric', last_name: 'Kelly')
+
+Meetup.create(name: "LaMonte & Kelly's Super Awesome Meetup", description: "The best meetup of all time", location: "Launch Academy Mission Control")
+Meetup.create(name: "SpaceJam", description: "You already know.", location: "Space")
+Meetup.create(name: "Donut Day", description: "Free donuts~!", location: "Everywhere.")
